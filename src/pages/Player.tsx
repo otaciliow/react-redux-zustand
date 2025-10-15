95e66275b48c28238be49f42ca9d5f583ca3f2db
otaciliow <otacilio.dml@hotmail.com>
2025-10-15 20:32:02 -0300

    fix: scrollbar warning

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -14,10 +14,11 @@ export function VideoContainer() {
                     <div className="flex-1">
                         <VideoPlayer />
                     </div>
-                    <aside className="w-80 absolute top-0 bottom-0 right-0 border-l divide-y-2 divide-zinc-900 border-zinc-800 bg-zinc-900 overflow-y-scroll scrollbar scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
+                    <aside className="w-80 absolute top-0 bottom-0 right-0 border-l divide-y-2 divide-zinc-900 border-zinc-800 bg-zinc-900 overflow-y-scroll scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
                         <Module moduleIndex={0} title="Desvendando Redux" amountOfLessons={3} />
                         <Module moduleIndex={1} title="Desvendando Redux" amountOfLessons={3} />
                         <Module moduleIndex={2} title="Desvendando Redux" amountOfLessons={3} />
+                        <Module moduleIndex={3} title="Desvendando Redux" amountOfLessons={3} />
                     </aside>
                 </main>
             </div>
